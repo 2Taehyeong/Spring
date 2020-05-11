765b44376acf3b340c7d5c15530aaad1ea32fe51
2Taehyeong <thlee0700@gmail.com>
2020-05-12 00:16:53 +0900

    8주차 실습

--- MyProject/src/main/java/kr/inhatc/spring/board/controller/BoardController.java
+++ MyProject/src/main/java/kr/inhatc/spring/board/controller/BoardController.java
@@ -2,6 +2,8 @@ package kr.inhatc.spring.board.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,13 +13,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.servlet.ModelAndView;
+
 
 import kr.inhatc.spring.board.dto.BoardDto;
 import kr.inhatc.spring.board.service.BoardService;
 
 @Controller
 public class BoardController {
+	
+	private Logger log = LoggerFactory.getLogger(this.getClass());
 	
 	@Autowired
 	private BoardService boardService;
@@ -41,6 +47,7 @@ public class BoardController {
 	@RequestMapping("/board/boardList")
 	public String boardList(Model model) {
 		List<BoardDto> list = boardService.boardList();
+		log.debug("=========>" + list.size());
 //		System.out.println("=========>" + list.size());
 		model.addAttribute("list", list);
 		return "board/boardList";
@@ -52,8 +59,8 @@ public class BoardController {
 	}
 	
 	@RequestMapping("/board/boardInsert")
-	public String boardInsert(BoardDto board) {
-		boardService.boardInsert(board);
+	public String boardInsert(BoardDto board, MultipartHttpServletRequest multipartHttpServletRequest) {
+		boardService.boardInsert(board, multipartHttpServletRequest);
 		return "redirect:/board/boardList";
 	}
 	
